9efa97bd2d51f103e3bc5731911fecd18e557cc4
NicKAnderson038 <nick7anderson@protonmail.com>
2024-04-27 16:02:11 -0700

    🎨 qr code carlos & nicole

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import { useState } from 'react'
+import QRCodeStyling from 'qr-code-styling'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import logo from './assets/anderson.png'
 import './App.css'
+
+const qrCode = new QRCodeStyling({
+    width: 500,
+    height: 500,
+    type: 'png',
+    data: "https://www.andersongaragedoorservices.com/",
+    image: logo,
+    dotsOptions: {
+        color: '#ffffff',
+        type: 'rounded',
+    },
+    backgroundOptions: {
+        color: "#000000",
+    },
+    cornersSquareOptions: {
+        color: '#FF5F15',
+        // type: "rounded"
+    },
+    imageOptions: {
+        crossOrigin: 'anonymous',
+        margin: 5,
+    },
+})
 
 function App() {
     const [count, setCount] = useState(0)
+
+    function qrcode() {
+        qrCode.download({ name: 'qr-code', extension: 'png' })
+    }
 
     return (
         <>
@@ -28,6 +57,9 @@ function App() {
                 <p>
                     Edit <code>src/App.jsx</code> and save to test HMR
                 </p>
+                <button onClick={qrcode}>
+                    Download QR Code
+                </button>
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
